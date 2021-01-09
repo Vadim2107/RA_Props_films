0784170435e403be8caeb973b72772b65c9b9eb2
Vadim2107 <58651870+Vadim2107@users.noreply.github.com>
2021-01-09 21:06:05 +0300

    edit stars

--- src/components/Stars.js
+++ src/components/Stars.js
@@ -1,29 +1,15 @@
 import React from 'react';
 import Star from './Star';
-// import shortId from 'shortid';
-
-// const initStar = (items) => items.map((value) => ({
-//   id: shortId.generate(),
-//   value,
-// }));
 
 export default function Stars(props) {
   const { count } = props;    
     if (count > 0 && count <= 5) {    
       return (        
-        <ul className="card-body-stars u-clearfix">
-          {/* <li key={shortId.generate()}><Star count={count} /></li> */}
+        <ul className="card-body-stars u-clearfix">          
           <Star count={count} />
         </ul>
       )
     }
 
-  return null;
-  // return (
-  //   <div></div>
-  // )  
-}
-
-Stars.defaultProps = {
-  count: 0,
+  return null;  
 }
